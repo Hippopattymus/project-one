3b6eebec6951f9d6fb5f4f0689f966326d0c47c2
John Mateer <jj.mateer@live.com>
2019-07-27 14:04:01 -0700

    Basic styling and javascript

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,90 +1,107 @@
-var gifs = ["Cats", "Memes", "Food", "Sleep"];
+var gifs = [];
 var results;
 var gif;
-
+var x;
 function displayInfo() {
-    gif = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        gif + "&api_key=S0s43WOAT4jzNdL4GICYdNrVfDl0MWMH&limit=10";
-
+    var repURL = "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyAxErsgI2POFlBroc_QuXNof9gx4cOtzpg&address=9901%20Jetmar%20Way.%20Elk%20Grove%20CA";
     $.ajax({
-        url: queryURL,
+        url: repURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-        results = response.data;
-        renderGifs();
+        results = response;
+        resultsURL = response.officials[0].photoUrl;
+        resultsName = response.officials[0].name;
+        
+        console.log(results);
+        
+        console.log(response.officials[0].photoUrl);
+        $(".politician-display").empty();
+        for (var i = 0; i < 2; i++) {
+    
+            var gifDiv = $("<div class = 'card'>");
+    
+            //var rating = results[i].rating;
+            var p = $("<p class = 'ml-1'>").text(response.offices[0].name);
+            p.append(":<br>" + resultsName);
+            p.addClass("float-left");
+    
+            var img = $("<img height = 100px width = 100px>");
+            img.attr("src", resultsURL);
+            
+            gifDiv.addClass("float-left");
+           // gifDiv.addClass("col-6")
+            gifDiv.addClass("pol");
+            gifDiv.prepend(p);
+            gifDiv.prepend(img);
+    
+            $(".politician-display").prepend(gifDiv);
+           
+        }
     });
 }
-
 // function renderButtons() {
-//     $("#buttons-view").empty();
-//     for (var i = 0; i < gifs.length; i++) {
-//         var a = $("<button class = 'rounded mr-1'>");
+//     $(".left-bar").empty();
+//     for (var i = 0; i < results.elections.length; i++) {
+//         var a = $("<button class = 'rounded mr-1 col-12'>");
 //         a.addClass("movie");
-
 //         //a.addClass("btn-info");
 //         a.attr("data-name", gifs[i]);
-//         a.text(gifs[i]);
-//         $("#buttons-view").append(a);
+       
+//         //a.text(electionResults);
+//         $(".left-bar").append(a);
 //     }
 // }
-
-function renderGifs() {
-    $(".politician-display").empty();
-    for (var i = 0; i < results.length; i++) {
-
-
-
-        var gifDiv = $("<div class = 'card'style=' width:276px'>");
-
-        var rating = results[i].rating;
-        var p = $("<p class = 'ml-1'>").text("Rating: " + rating);
-
-        var img = $("<img>");
-        img.attr("src", results[i].images.fixed_height_still.url);
-
-        img.attr("data-still", results[i].images.fixed_height_still.url);
-        img.attr("data-animate", results[i].images.fixed_height.url);
-        img.attr("data-state", "still");
-        img.attr("class", "still");
-
-        console.log(results[i].images.fixed_height_still.url);
-        gifDiv.addClass("float-left")
-        //gifDiv.addClass("col-2")
-        gifDiv.addClass("gif");
-        gifDiv.prepend(p);
-        gifDiv.prepend(img);
-
-        $(".politician-display").prepend(gifDiv);
-    }
-}
 $("#add-politician").on("click", function (event) {
     event.preventDefault();
-    var gif = $("#politician-input").val();
+    gif = $("#politician-input").val();
     $("#politician-input").val("");
     gifs.push(gif);
-    gifs.attr("date-state", gifs[i]);
-    // renderButtons();
     displayInfo();
-
 });
-
-$(document).on("click", ".still", function (event) {
-
-    var state = $(this).attr("data-state");
-    console.log(state);
-    if (state == "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-    } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
+// $(document).on("click", ".still", function (event) {
+//     var state = $(this).attr("data-state");
+//     console.log(state);
+//     if (state == "still") {
+//         $(this).attr("src", $(this).attr("data-animate"));
+//         $(this).attr("data-state", "animate");
+//     } else {
+//         $(this).attr("src", $(this).attr("data-still"));
+//         $(this).attr("data-state", "still");
+//     }
+// });
+var electionsURL = "https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyAxErsgI2POFlBroc_QuXNof9gx4cOtzpg";
+$.ajax({
+    url: electionsURL,
+    method: "GET"
+}).then(function (response) {
+    var electionResults = response;
+    
+    console.log(electionResults);
+    
+   //console.log(electionResults.officials[0].name);
+   // renderGifs();
+    //renderButtons();
+    $(".left-bar").empty();
+    for (var i = 0; i < electionResults.elections.length; i++) {
+        var a = $("<button class = 'rounded mr-1 col-12'>");
+        a.addClass("elections-" +i);
+        a.attr("val", i);
+        x = $('<button class = x rounded>');
+        x.attr("val", i);
+        x.text("X");
+        
+        
+        //a.addClass("btn-info");
+        a.attr("data-name", gifs[i]);
+   
+        a.text(electionResults.elections[i].electionDay);
+        a.append(x);
+        $(".left-bar").prepend(a);
+}
 });
-
-$(document).on("click", ".movie", displayInfo);
-
-
-
-// renderButtons();
+function deleteBtn() {
+    var del = $(this).attr("val");
+    $(".elections-"+del).empty();
+}
+//delete button
+$(document).on("click", ".x", deleteBtn);
